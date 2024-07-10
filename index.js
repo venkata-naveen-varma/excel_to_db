8aefc4a2e62878eb6ef2f29de57fdc9cffc34bfe
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-07-10 23:16:32 +0530

    db connection

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from 'express';
 
 require('dotenv').config();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json());
+
+app.get('/test', (req, res)=>{
+    console.log("In /test");
+    var msg = "testing";
+    res.json({msg});
+})
