98d739fbde460c9ecf3073c044f32e211572cd4f
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-07-11 14:00:52 +0530

    db connect

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import pool from "./src/utils/db_connection.js";
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: false })); 
-app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/test', (req, res)=>{
     console.log("In /test");
     var msg = "testing";
+    
+    const query = 'SELECT * FROM testtable1'
+    
+    const result = pool.query(query, (err, result) => {
+        if (err) return console.log(err);
+        console.log(result);
+        res.send(result);
+    });
     res.json({msg});
-})
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
